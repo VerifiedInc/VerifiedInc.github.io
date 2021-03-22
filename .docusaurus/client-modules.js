3e370ebdf6ebe45e243ec12ca82f6d4c1443bafb
Ray Smets <rayjsmets@gmail.com>
2021-03-22 14:29:39 -0700

    Deploy website - based on 58b648d97e1d183cb0b3b3e09f76ea5e41a4d48d

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/raysmets/dev/UnumID.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raysmets/dev/UnumID.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raysmets/dev/UnumID.github.io/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/raysmets/dev/UnumID.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/raysmets/dev/UnumID.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/raysmets/dev/UnumID.github.io/src/css/custom.css"),
-];

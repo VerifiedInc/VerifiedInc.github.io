96f05077d137320011c8bb68583a177451e54509
Iagor Moraes <iagor2014@outlook.com>
2023-07-03 10:13:50 -0300

    fix: remove regex with lookbehind unsupported by safari

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,13 @@
 // This pattern will split camel cased string out of the credential,
 // when a camel word contain a sequence of uppercase character it is kept to maintain consistency of that word.
-const schemaNamePattern = /(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])/gm;
+const schemaNamePattern = /([A-Z][a-z0-9]+)/gm;
 
 // Format the camel cased text to a human-readable.
-export const prettyField = (field) => field.split(schemaNamePattern).join(' ');
+export const prettyField = (field) =>
+  field
+    .split(schemaNamePattern)
+    .filter((e) => e)
+    .join(' ');
 
 // Return an object literal with key as values.
 export const keysLiteral = (object) =>

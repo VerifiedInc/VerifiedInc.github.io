70d7899371f5cb80786bdb6e3d02a99cb2acb496
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-12-05 17:55:41 -0300

    refactor: replace Button components with PostmanButton and SwaggerButton

--- src/components/EndpointHeader.jsx
+++ src/components/EndpointHeader.jsx
@@ -1,48 +1,14 @@
 import PropTypes from 'prop-types';
-import Button from '@mui/material/Button';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+
+import PostmanButton from './Buttons/PostmanButton.jsx';
+import SwaggerButton from './Buttons/SwaggerButton.jsx';
 
 const EndpointHeader = ({ children, postmanUrl, swaggerUrl }) => (
     <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'top' }}>
         {children}
         <div>
-            <Button
-                href={swaggerUrl}
-                target="_blank"
-                variant='outlined'
-                size="small"
-                startIcon={<OpenInNewIcon />}
-                sx={{
-                    color: '#6EAD3A',
-                    borderColor: '#6EAD3A',
-                    '&:hover': {
-                        backgroundColor: '#6EAD3A',
-                        color: '#FFFFFF',
-                        borderColor: '#6EAD3A',
-                    }
-                }}
-            >
-                Run in Swagger
-            </Button>
-            <Button
-                href={postmanUrl}
-                target="_blank"
-                variant="outlined"
-                size="small"
-                startIcon={<OpenInNewIcon />}
-                sx={{
-                    ml: 1,
-                    color: '#FF6C37',
-                    borderColor: '#FF6C37',
-                    '&:hover': {
-                        backgroundColor: '#FF6C37',
-                        color: '#FFFFFF',
-                        borderColor: '#FF6C37',
-                    }
-                }}
-            >
-                Run in Postman
-            </Button>
+            <SwaggerButton url={swaggerUrl} />
+            <PostmanButton url={postmanUrl} />
         </div>
     </div>
 );

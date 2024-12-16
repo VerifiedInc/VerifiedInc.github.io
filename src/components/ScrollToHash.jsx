b999cc5d48ba846bee3a07b5e29cf5d2a1b6de89
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-12-16 17:15:55 -0300

    Refactor ScrollToHash component to improve hash scrolling logic and manage mounted state

--- src/components/ScrollToHash.jsx
+++ src/components/ScrollToHash.jsx
@@ -3,22 +3,25 @@ import { useEffect, useState } from "react";
 
 const ScrollToHash = () => {
     const location = useLocation();
-    const [firstRender, setFirstRender] = useState(false);
+    const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
-        if (location.hash && !firstRender) {
-            console.log(location.hash);
-            // Remove the '#' from the hash to get the element ID
-            const hash = location.hash.substring(1);
-            // Find the element with the corresponding ID
-            setTimeout(() => {
-                const element = document.getElementById(hash);
-                // If the element exists, scroll to it
-                if (element) element.scrollIntoView({ behavior: "smooth" });
-            }, 100);
-            setFirstRender(true);
+        // When the component mounts, check if there's a hash in the URL
+        // If there is, set the mounted state to true so we can scroll to it
+        if (!mounted && location.hash) {
+            setMounted(true);
         }
-    }, [location.hash]); // Run this effect whenever the hash changes
+    }, [location.hash]);
+
+    useEffect(() => {
+        if (!mounted) return;
+
+        // Remove the '#' from the hash to get the element ID
+        const hash = location.hash.substring(1);
+        const element = document.getElementById(hash);
+        // If the element exists, scroll to it
+        if (element) element.scrollIntoView({ behavior: "smooth" });
+    }, [mounted]); // Run this effect whenever the component is mounted
 
     return null; // This component doesn't render anything
 };

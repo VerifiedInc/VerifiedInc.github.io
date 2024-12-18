0a6ed1cf34f697e33c4d09058cbf6de36572dee4
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-12-18 10:25:03 -0300

    feat: remove button icon and shadows

--- src/components/Buttons/NavbarButton.jsx
+++ src/components/Buttons/NavbarButton.jsx
@@ -1,5 +1,3 @@
-import { Dashboard, DashboardOutlined } from '@mui/icons-material';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import Button from '@mui/material/Button';
 import React from 'react';
 
@@ -11,12 +9,15 @@ export default function NavbarButton({ to, label, }) {
       target="_blank"
       color="primary"
       variant='contained'
-      startIcon={<Dashboard sx={{ color: 'white' }} />}
       sx={{
         fontSize: '16px',
         fontWeight: '900 !important',
         borderRadius: '8px',
         textTransform: 'none',
+        boxShadow: 'none',
+        '&:hover': {
+          boxShadow: 'none',
+        }
       }}
     >{label}</Button>
   );

ad6ca9d7507e520134b025cc0560fdb5e617cfec
Liam McCarty <liammccarty@macbook-pro-7.tail859ae.ts.net>
2025-02-03 16:44:44 -0500

    Updating risk signals info, adding firewall errors, removing deprecated metadata, updating Gender and removing Sex credentials

--- src/components/ErrorDisplay.jsx
+++ src/components/ErrorDisplay.jsx
@@ -4,12 +4,13 @@ import { Box } from '@mui/material';
 import Admonition from '@theme/Admonition';
 import IntegrationTypeErrorsAdmonition from '@site/versioned_docs/version-2/reusables/integration-type-errors-admonition.mdx'
 
-const ErrorDisplay = ({ message, description, additionalInputs, inputAttemptsExceeded, integrationType, whenThisIsReturned, howToHandle, youShouldNeverGetThisError, children: code, sx }) => {
+const ErrorDisplay = ({ message, description, additionalInputs, riskSignals, inputAttemptsExceeded, integrationType, whenThisIsReturned, howToHandle, youShouldNeverGetThisError, children: code, sx }) => {
     const markdownMessage = `\`"${message}"\``;
     const tableRowMessage = message ? <tr><th><Markdown>`messsage`</Markdown></th><td><Markdown>{markdownMessage}</Markdown></td></tr> : null;
     const markdownDescription = description ? `> ${description}` : null;
     const markdownAdditionalInputs = additionalInputs ? `\`${additionalInputs}\`` : null;
     const tableRowAdditionalInputs = additionalInputs ? <tr><th><Markdown>`data.additionalInputs`</Markdown></th><td><Markdown>{markdownAdditionalInputs}</Markdown></td></tr> : null;
+    const tableRowRiskSignals = riskSignals ? <tr><th><Markdown>`data.riskSignals`</Markdown></th><td><Markdown>[`RiskSignals`](./types#risksignals)</Markdown></td></tr> : null;
     const markdownInputAttemptsExceeded = inputAttemptsExceeded ? `\`${inputAttemptsExceeded}\`` : null;
     const tableRowMarkdownInputAttemptsExceeded = inputAttemptsExceeded ? <tr><th><Markdown>`data.inputAttemptsExceeded`</Markdown></th><td><Markdown>{markdownInputAttemptsExceeded}</Markdown></td></tr> : null;
     const integrationTypeAdmonition = integrationType ? <IntegrationTypeErrorsAdmonition option={integrationType}/> : null;
@@ -28,6 +29,7 @@ const ErrorDisplay = ({ message, description, additionalInputs, inputAttemptsExc
                     {tableRowMessage}
                     {tableRowAdditionalInputs}
                     {tableRowMarkdownInputAttemptsExceeded}
+                    {tableRowRiskSignals}
                 </tbody>
             </table>
 

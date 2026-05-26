7304c5632e4b94438c06435114652f65f1843775
Liam Hale McCarty <37675947+liamhalemccarty@users.noreply.github.com>
2026-05-26 16:41:10 -0400

    statusPage (#336)

--- src/theme/NavbarItem/ComponentTypes.js
+++ src/theme/NavbarItem/ComponentTypes.js
@@ -1,7 +1,9 @@
 import ComponentTypes from '@theme-original/NavbarItem/ComponentTypes';
 import NavbarButton from '@site/src/components/Buttons/NavbarButton';
+import NavbarStatus from '@site/src/components/NavbarStatus';
 
 export default {
   ...ComponentTypes,
   'custom-navbarButton': NavbarButton,
+  'custom-navbarStatus': NavbarStatus,
 };

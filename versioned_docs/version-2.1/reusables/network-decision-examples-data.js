c297b732318a8e3cf8840e2e704329b392ef5946
Liam Hale McCarty <37675947+liamhalemccarty@users.noreply.github.com>
2026-09-16 18:51:29 -0400

    Editing based on Leo's input (#368)
    Co-authored-by: Leonardo Adriano <leo.adriano1994@gmail.com>

--- versioned_docs/version-2.1/reusables/network-decision-examples-data.js
+++ versioned_docs/version-2.1/reusables/network-decision-examples-data.js
@@ -1,21 +1,21 @@
 import { indentJson } from '@site/versioned_docs/version-2.1/reusables/json-utils.js';
 import { networkRuleInNetworkJson, networkRuleOutOfNetworkJson } from '@site/versioned_docs/version-2.1/reusables/network-rules-examples-data.js';
 
-export const networkStatusInNetworkJson = `{
+export const networkDecisionInNetworkJson = `{
     "status": "IN_NETWORK",
     "rules": [
         ${indentJson(networkRuleInNetworkJson, 8)}
     ]
 }`;
 
-export const networkStatusOutOfNetworkJson = `{
+export const networkDecisionOutOfNetworkJson = `{
     "status": "OUT_OF_NETWORK",
     "rules": [
         ${indentJson(networkRuleOutOfNetworkJson, 8)}
     ]
 }`;
 
-export const networkStatusIndeterminateJson = `{
+export const networkDecisionIndeterminateJson = `{
     "status": "INDETERMINATE",
     "rules": [
         ${indentJson(networkRuleInNetworkJson, 8)},
--- versioned_docs/version-2.1/reusables/parsed-edi-271-examples-data.js
+++ versioned_docs/version-2.1/reusables/parsed-edi-271-examples-data.js
@@ -1,6 +1,7 @@
 export const parsedEdi271AetnaJson = `{
     "coverageStatus": "active",
     "coverageType": ["medical"],
+    "serviceTypeCodes": ["30"],
     "payer": {
         "name": "Aetna",
     },
@@ -11,19 +12,28 @@ export const parsedEdi271AetnaJson = `{
             "lastName": "Hendricks"
         },
         "birthDate": "1989-08-01",
-        "sex": "Male"
+        "sex": "Male",
+        "address": {
+            "line1": "5320 Newell Rd",
+            "line2": "",
+            "city": "Palo Alto",
+            "state": "CA",
+            "zipCode": "94303"
+        }
     },
     "plan": {
         "name": "Managed Choice Open Access",
         "groupNumber": "123456-123-12345",
-        "insuranceType": "C1",
+        "groupName": "PiedPiper",
+        "insuranceTypeCodes": ["C1"],
         "effectiveDate": "2026-01-01",
         "terminationDate": "2026-12-31"
     },
     "benefits": [
         {
             "type": "deductible",
-            "serviceTypeCodes": "[30]",
+            "serviceTypeCodes": ["30"],
+            "insuranceTypeCode": "C1",
             "level": "individual",
             "network": "in",
             "amount": "1500.00",
@@ -35,7 +45,7 @@ export const parsedEdi271AetnaJson = `{
     "contacts": [
         {
             "name": "Aetna Member Services",
-            "serviceTypeCodes": "[30]",
+            "serviceTypeCodes": ["30"],
             "phone": "8005550000",
             "fax": "8005550001",
             "email": "memberservices@aetna.com",
@@ -48,6 +58,7 @@ export const parsedEdi271AetnaJson = `{
 export const parsedEdi271UnitedHealthcareJson = `{
     "coverageStatus": "active",
     "coverageType": ["medical"],
+    "serviceTypeCodes": ["30"],
     "payer": {
         "name": "UnitedHealthcare",
     },
@@ -58,19 +69,28 @@ export const parsedEdi271UnitedHealthcareJson = `{
             "lastName": "Hendricks"
         },
         "birthDate": "1989-08-01",
-        "sex": "Male"
+        "sex": "Male",
+        "address": {
+            "line1": "5320 Newell Rd",
+            "line2": "",
+            "city": "Palo Alto",
+            "state": "CA",
+            "zipCode": "94303"
+        }
     },
     "plan": {
         "name": "Choice Plus",
         "groupNumber": "1234567",
-        "insuranceType": "C1",
+        "groupName": "PiedPiper",
+        "insuranceTypeCodes": ["C1"],
         "effectiveDate": "2026-01-01",
         "terminationDate": "2026-12-31"
     },
     "benefits": [
         {
             "type": "deductible",
-            "serviceTypeCodes": "[30]",
+            "serviceTypeCodes": ["30"],
+            "insuranceTypeCode": "C1",
             "level": "individual",
             "network": "in",
             "amount": "1500.00",
@@ -82,7 +102,7 @@ export const parsedEdi271UnitedHealthcareJson = `{
     "contacts": [
         {
             "name": "UnitedHealthcare Member Services",
-            "serviceTypeCodes": "[30]",
+            "serviceTypeCodes": ["30"],
             "phone": "8005550002",
             "fax": "8005550003",
             "email": "memberservices@uhc.com",
